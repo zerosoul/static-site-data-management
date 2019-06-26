b39c49394d373d0d850a68c7367c83128d01cf56
tristan <yangguochun@ddweilai.com>
2019-06-26 15:41:10 +0800

    feat: change db source

--- app.js
+++ app.js
@@ -31,9 +31,10 @@ app.use(
     graphiql: true
   })
 );
-const mdb_conn_str = `mongodb+srv://${process.env.MONGO_USER}:${
-  process.env.MONGO_PASSWORD
-}@cluster-yanggc-v65dk.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`;
+// const mdb_conn_str = `mongodb+srv://${process.env.MONGO_USER}:${
+//   process.env.MONGO_PASSWORD
+// }@cluster-yanggc-v65dk.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`;
+const mdb_conn_str = `mongodb://114.242.25.9:27117/test`;
 console.info("conn str", mdb_conn_str);
 mongoose
   .connect(mdb_conn_str, {
